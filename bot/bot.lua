15b32642bb73a75e51ac14fba0a2dd8a20038005
blackops97 <blackops97@users.noreply.github.com>
2017-03-08 22:00:50 +0300

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -1,6 +1,3 @@
--- #Beyond Reborn Robot
--- #@BeyondTeam
-
 tdcli = dofile('./tg/tdcli.lua')
 serpent = (loadfile "./libs/serpent.lua")()
 feedparser = (loadfile "./libs/feedparser.lua")()
@@ -71,37 +68,20 @@ function create_config( )
 		"fun",
 		"info"
 	},
-    sudo_users = {157059515},
+    sudo_users = {113644348},
     admins = {},
     disabled_channels = {},
     moderation = {data = './data/moderation.json'},
-    info_text = [[》Beyond Reborn v4.0
+    info_text = [[》TEAMEvil v6
 An advanced administration bot based on https://valtman.name/telegram-cli
 
-》https://github.com/BeyondTeam/BDReborn 
+》https://github.com/blackops97/TEAMEvil 
 
-》Admins :
-》@SoLiD ➣ Founder & Developer《
-》@Makan ➣ Developer《
-》@Rixel ➣ Developer 《
-》@Exacute ➣ Developer《
-》@To0fan ➣ Developer《
-》@CiveY ➣ Developer
-》@ArmanDev ➣ Manager《
-》@MrPars ➣ Manager《
-
-》Special thanks to :
-》@Vysheng
-》@MrHalix
-》@K_a_I_i_I_i_n_u_x
-》@Nero_Dev
-》And Beyond Team Members
 
 》Our channel :
-》@BeyondTeam《
+》@TEAMEvil《
 
-》Our website :
-》http://BeyondTeam.ir
+
 ]],
   }
 	serialize_to_file(config, './data/config.lua')
