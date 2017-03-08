21927d0eac3a7b62b5f9bd6eac158d4b137035e2
blackops97 <blackops97@users.noreply.github.com>
2017-03-08 23:16:57 +0300

    Update TEAMEvil.sh

--- TEAMEvil.sh
+++ TEAMEvil.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/env bash 
+
+#!/usr/bin/env bash
 
 cd $HOME/TEAMEvil
 
@@ -23,33 +24,6 @@ install() {
 		chmod +x tg
 }
 
-function print_logo() {
-	echo -e "\033[38;5;600m"
-	echo -e "          ____  ____     _____"
-	echo -e "         |  _ )|  _ \   |_   _|___ ____   __  __"
-	echo -e "         |  _ \| |_) )    | |/ .__|  _ \_|  \/  |"
-	echo -e "         |____/|____/     |_|\____/\_____|_/\/\_|"
-	echo -e "\n\e[36m"
-}
-
-function logo_play() {
-    declare -A txtlogo
-    seconds="0.010"
-    txtlogo[1]=" ____  ____     _____"
-    txtlogo[2]="|  _ )|  _ \   |_   _|___ ____   __  __"
-    txtlogo[3]="|  _ \| |_) )    | |/ .__|  _ \_|  \/  |"
-    txtlogo[4]="|____/|____/     |_|\____/\_____|_/\/\_|"
-    printf "\033[38;5;600m\t"
-    for i in ${!txtlogo[@]}; do
-        for x in `seq 0 ${#txtlogo[$i]}`; do
-            printf "${txtlogo[$i]:$x:1}"
-            sleep $seconds
-        done
-        printf "\n\t"
-    done
-    printf "\n"
-}
-
 if [ "$1" = "install" ]; then
   install
   else
@@ -58,20 +32,18 @@ if [ ! -f ./tg/tgcli ]; then
     echo "tg not found"
     echo "Run $0 install"
     exit 1
- fi
+fi
 
-
-   print_logo
+if [ "$1" = "-p" ]; then
+    echo -e "\033[38;5;208m"
+    echo -e "     >> TEAMEvil Source :BY @TH3_Evil "
+    echo -e "                                              \033[0;00m"
+    echo -e "\e[36m"
+    ./tg/tgcli -s ./bot/bot.lua -p $2
+fi
    echo -e "\033[38;5;208m"
-   echo -e "     >>>>                       We Are Not Attacker                             "
-   echo -e "     >>>>                       We Are Not Alliance                             "
-   echo -e "     >>>>                       We Are Family                                   "
-   echo -e "     >>>>                       We Are The Best :-)                             "
-   echo -e "     >>>>                       @TEAMEvil                                   "
-   echo -e "\033[0;00m"
+   echo -e "     >> TEAMEvil Source :BY @TH3_Evil "
+   echo -e "                                              \033[0;00m"
    echo -e "\e[36m"
-   logo_play
-   #sudo service redis-server restart
-   #./tg/tgcli -s ./bot/bot.lua -l 1 -E $@
-   ./tg/tgcli -s ./bot/bot.lua $@
+   ./tg/tgcli -s ./bot/bot.lua -l 1 -E $@
 fi
